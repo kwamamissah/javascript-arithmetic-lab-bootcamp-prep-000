4e572599bccc1cf4c110cc904f3b6fd0f0939ef9
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-07 18:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var a = Math.floor(Math.random() * 1000);
 var b = Math.floor(Math.random() * 1000);
 
 function add(a,b) {
-  return a+b;
+  return a + b;
 }
 
 function subtract(a,b) {
-  return a-b;
+  return a - b;
 }
